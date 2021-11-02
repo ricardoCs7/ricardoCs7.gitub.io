8e6245cfcf1ab1ebcadcbd3f8140d404d7c0fe94
ricardoCs7 <r.carrasco05@ufromail.cl>
2021-11-02 18:16:36 -0300

    nuevo

---
+++ src/components/PopularMovies.vue
@@ -0,0 +1,31 @@
+<template >
+  <section class="movie-grid">
+    <MPview v-for="movie in movies" :movie="movie" />
+  </section>
+</template>
+<script>
+import apiService from "@/services/api.service";
+import MPview from "./MPview.vue";
+
+export default {
+  components: { MPview },
+  data() {
+    return {
+      movies: [],
+    };
+  },
+  mounted() {
+    this.getPopular();
+  },
+  methods: {
+    async getPopular() {
+      const { data } = await apiService.getPopular();
+      this.movies = data.results;
+      console.log(data);
+    },
+  },
+};
+</script>
+<style >
+
+</style>
---
+++ src/views/Popular.vue
@@ -0,0 +1,17 @@
+<template >
+  <div>
+    <h1 id="tit1">Popular Movies</h1>
+    <PopularMovies />
+  
+  </div>
+</template>
+<script>
+import PopularMovies from "../components/PopularMovies.vue";
+export default {
+  components: {
+    PopularMovies,
+  },
+};
+</script>
+<style >
+</style>
